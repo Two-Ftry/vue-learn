46ec81972d9be5e4a5afecd43093aec0d4dff927
suifengfengye <1527261080@qq.com>
2017-01-09 22:26:15 +0800

    准备mocha分支

--- src/common/js/common.js
+++
@@ -1,8 +0,0 @@
-/**
-* @desc common
-*/
-window.addEventListener('DOMContentLoaded', function(){
-  var bodyWidth = $('html, body').width();
-  var fontSize = bodyWidth / 6.4;
-  $('html').css('font-size', fontSize);
-}, false);
