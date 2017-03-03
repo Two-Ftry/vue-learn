cafe89ec1021b583a95f67c04a31964ec7411cb3
jianfeng_huang <jianfeng_huang@kingdee.com>
2017-03-03 09:57:26 +0800

    ssr done

--- assets/myApp.js
+++ assets/myApp.js
@@ -1,25 +1,31 @@
-
-var Vue = require('vue');
-
-var createApp = function(){
-  return new Vue({
-    template: '<div id="app">你已经在页面上花费{{count}}秒</div>',
-    data: function(){
-      return {
-        count: 0
-      };
-    },
-    mounted: function(){
-      this.tickCount();
-    },
-    methods: {
-      tickCount: function(){
-        var me = this;
-        me.count ++;
-        setTimeout(function(){
+var app;
+(function(){
+  var createApp = function(){
+    return new Vue({
+      template: '<div id="app">你已经在页面上花费{{count}}秒</div>',
+      data: function(){
+        return {
+          count: 0
+        };
+      },
+      mounted: function(){
+        this.tickCount();
+      },
+      methods: {
+        tickCount: function(){
+          var me = this;
           me.count ++;
-        }, 1000);
+          setInterval(function(){
+            me.count ++;
+          }, 1000);
+        }
       }
-    }
-  });
-};
+    });
+  };
+
+  if(typeof window === 'undefined'){
+    module.exports = createApp;
+  }else{
+    app = createApp();
+  }
+})();
--- bin/myServer.js
+++ bin/myServer.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var Vue = require('vue');
+var renderer = require('vue-server-renderer').createRenderer();
 var app = express();
+
+// 定义全局的Vue为了服务端的app.js
+global.Vue = require('vue');
 
 app.use('/assets', express.static(path.resolve(__dirname, '../assets')));
 
 var layout = fs.readFileSync('./myIndex.html', 'utf-8');
 
+var myApp = require('../assets/myApp')();
+
 app.get('*', function(req, res){
-  res.send(layout);
+  renderer.renderToString(myApp, function(err, html){
+      layout = layout.replace('<div id="app"></div>', html);
+      console.log(layout);
+      res.send(layout);
+  });
 });
 
 app.listen(5001);
